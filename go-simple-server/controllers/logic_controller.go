59ee2d4027b80bd0f5f114dc9f6fa6fb2786d7f2
Nuper <danielanper@hotmail.com>
2020-01-11 14:43:01 +0100

    asdd

--- go-simple-server/controllers/logic_controller.go
+++ go-simple-server/controllers/logic_controller.go
@@ -4,8 +4,11 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"encoding/json"
+	"fmt"
+	"github.com/Nuper/DerezWP/go-simple-server/models"
 )
-// DoMainRequest : retrieves main chunk of data
+// DoMainRequest : retrieves SOURCE data
 func DoMainRequest() []byte {
 	newReq, anyError := http.NewRequest("GET", "http://api.pathofexile.com/public-stash-tabs", nil)
 	//571783755-589642300-558214129-635578319-604141055
@@ -29,4 +32,17 @@ func DoMainRequest() []byte {
 		log.Fatalln(err)
 	}
 	return body
+}
+// FindItemsByName : Finds items by name in the SOURCE
+func FindItemsByName() []models.Item{
+	rawData := DoMainRequest()
+	println(string(rawData[0:300]))
+
+	var f models.FullResponse
+	json.Unmarshal(rawData, &f)
+	var g []models.Item
+	for i := 0; i < len(f.Stashes); i++{
+		fmt.Println(f.Stashes[i])
+	}
+	return g
 }
